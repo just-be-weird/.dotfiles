0b1377d0c69d12c1727086fdd57f60febc6b25f4
just-be-weird <17427511+just-be-weird@users.noreply.github.com>
2022-10-28 23:26:21 +0530

    Few more fixes in LSP configs

--- neovim/nvim/lua/just-be-weird/plugins/lsp/lspconfig.lua
+++ neovim/nvim/lua/just-be-weird/plugins/lsp/lspconfig.lua
@@ -1,104 +1,162 @@
 -- import lspconfig plugin safely
 local lspconfig_status, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status then
-	return
+    return
 end
 
 -- import cmp-nvim-lsp plugin safely
 local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not cmp_nvim_lsp_status then
-	return
+    return
 end
 
 -- import typescript plugin safely
 local typescript_setup, typescript = pcall(require, "typescript")
 if not typescript_setup then
-	return
+    return
 end
 
-local keymap = vim.keymap -- for conciseness
+local keymap = vim.keymap.set -- for conciseness
 
 -- enable keybinds only for when lsp server available
 local on_attach = function(client, bufnr)
-	-- keybind options
-	local opts = { noremap = true, silent = true, buffer = bufnr }
+    -- keybind options
+    local opts = {
+        noremap = true,
+        silent = true,
+        buffer = bufnr
+    }
 
-	-- set keybinds
-	keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
-	keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
-	keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
-	keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
-	keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
-	keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
-	keymap.set("n", "<leader>d", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
-	keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
-	keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- jump to previous diagnostic in buffer
-	keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
-	keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
-	keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts) -- see outline on right hand side
+    -- set keybinds
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    keymap('n', '<leader>td', '<Cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
 
-	-- typescript specific keymaps (e.g. rename file and update imports)
-	if client.name == "tsserver" then
-		keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>") -- rename file and update imports
-		keymap.set("n", "<leader>oi", ":TypescriptOrganizeImports<CR>") -- organize imports (not in youtube nvim video)
-		keymap.set("n", "<leader>ru", ":TypescriptRemoveUnused<CR>") -- remove unused variables (not in youtube nvim video)
-	end
+    keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
+
+    keymap('n', '<leader>f', '<Cmd>function()vim.lsp.buf.format {async = true}end<CR>', opts)
+
+    keymap('n', '<leader>wa', '<Cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+
+    keymap('n', '<leader>wr', '<Cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+
+    keymap('n', '<leader>wl', '<Cmd>lua function() print(vim.inspect(vim.lsp.buf.list_workspace_folders()))end<CR>',
+        opts)
+
+    if client.name == 'tsserver' then
+        keymap('n', '<leader>rf', ':TypescriptRenameFile<CR>') -- rename file and update imports
+        keymap('n', '<leader>oi', ':TypescriptOrganizeImports<CR>') -- organize imports
+        keymap('n', '<leader>ru', ':TypescriptRemoveUnused<CR>') -- remove unused variables
+    end
 end
 
 -- used to enable autocompletion (assign to every lsp server config)
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
 -- Change the Diagnostic symbols in the sign column (gutter)
--- (not in youtube nvim video)
-local signs = { Error = " ", Warn = " ", Hint = "ﴞ ", Info = " " }
+local signs = {
+    Error = ' ',
+    Warn = ' ',
+    Hint = ' ',
+    Info = ' '
+}
 for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, {
+        text = icon,
+        texthl = hl,
+        numhl = ""
+    })
 end
 
 -- configure html server
 lspconfig["html"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
+    capabilities = capabilities,
+    on_attach = on_attach
 })
 
 -- configure typescript server with plugin
 typescript.setup({
-	server = {
-		capabilities = capabilities,
-		on_attach = on_attach,
-	},
+    server = {
+        capabilities = capabilities,
+        filetypes = {'typescript', 'typescriptreact', 'typescript.tsx'},
+        cmd = {'typescript-language-server', '--stdio'},
+        on_attach = on_attach
+    }
 })
+
+lspconfig['flow'].setup {
+    on_attach = on_attach,
+    capabilities = capabilities
+}
 
 -- configure css server
 lspconfig["cssls"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
+    capabilities = capabilities,
+    on_attach = on_attach
 })
 
 -- configure tailwindcss server
 lspconfig["tailwindcss"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
+    capabilities = capabilities,
+    on_attach = on_attach
 })
+
+lspconfig.astro.setup {
+    on_attach = on_attach,
+    capabilities = capabilities
+}
 
 -- configure lua server (with special settings)
 lspconfig["sumneko_lua"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-	settings = { -- custom settings for lua
-		Lua = {
-			-- make the language server recognize "vim" global
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				-- make language server aware of runtime files
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.stdpath("config") .. "/lua"] = true,
-				},
-			},
-		},
-	},
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = { -- custom settings for lua
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT'
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {'vim'}
+            },
+            workspace = {
+                -- make language server aware of runtime files
+                library = {
+                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                    [vim.fn.stdpath("config") .. "/lua"] = true
+                },
+                checkThirdParty = false
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false
+            }
+        }
+    }
+})
+
+lspconfig.sourcekit.setup {
+    on_attach = on_attach,
+    capabilities = capabilities
+}
+
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = true,
+    update_in_insert = false,
+    virtual_text = {
+        spacing = 2,
+        prefix = '●'
+    },
+    severity_sort = true
+})
+
+vim.diagnostic.config({
+    virtual_text = {
+        prefix = '●'
+    },
+    update_in_insert = true,
+    float = {
+        source = 'always' -- Or 'if_many'
+    }
 })
